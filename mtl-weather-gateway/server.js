cc314117931014891da8bc370b6f946c9506f12f
Mike Leonard <mtleonardjr@gmail.com>
2021-02-09 16:43:56 -0500

    added cors

--- mtl-weather-gateway/server.js
+++ mtl-weather-gateway/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const config = require('./resources/config')
 const logger = require('node-color-log');
+const cors = require('cors');
 
 const app = express();
 
 //Init middleware
 app.use(express.json());
+
+app.use(cors());
 
 //Define Routes
 app.use('/', require('./routes/route'));
